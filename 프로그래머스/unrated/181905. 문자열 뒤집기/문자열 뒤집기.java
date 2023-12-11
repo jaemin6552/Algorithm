0b4f5e7b559ea8f02f8525483ae8486c851d7f9e
jaemin6552 <jaemin6552@Naver.com>
2023-12-12 00:44:53 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 9.70 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,10 +1,8 @@
+import java.util.*;
 class Solution {
     public String solution(String my_string, int s, int e) {
-        String answer = my_string.substring(0,s);
-        for(int i =e; i>=s; i--){
-            answer+=my_string.charAt(i);
-        }
-        answer+=my_string.substring(e+1,my_string.length());
-        return answer;
+        StringBuilder answer = new StringBuilder(my_string.substring(s,e+1));
+        answer.reverse();
+        return my_string.substring(0,s)+answer+my_string.substring(e+1);
     }
 }
