fc67c1fea6a94c02e11e6fca1ee191b6797aae7c
jaemin6552 <jaemin6552@Naver.com>
2023-12-21 00:14:34 +0900

    [level 2] Title: 주식가격, Time: 12.16 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,17 +1,12 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-        Stack<int[]>stack = new Stack<>();
-        for(int i =0; i<prices.length; i++){
-            while(!stack.isEmpty() && stack.peek()[0] > prices[i]){
-                answer[stack.peek()[1]] = i-stack.peek()[1];
-                stack.pop(); 
+        int []answer = new int[prices.length];
+        for(int i =0; i<prices.length-1; i++){
+            for(int j = i+1; j<prices.length; j++){
+                answer[i]++;
+                if(prices[i] > prices[j]) break;
             }
-            stack.push(new int[]{prices[i],i});
-        }
-        for(int i=1; i<answer.length; i++){
-            if(answer[i-1] == 0) answer[i-1] = prices.length-i;
         }
         return answer;
     }
