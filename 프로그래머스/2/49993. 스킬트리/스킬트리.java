212e7b67633b265edf2b168fc76e58950540bdca
JM <jaemin6552@Naver.com>
2024-01-03 19:38:29 +0900

    [level 2] Title: 스킬트리, Time: 6.74 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -3,15 +3,16 @@ class Solution {
         int answer = 0;
         for(int i =0; i<skill_trees.length; i++){
             boolean isTrue = true;
-            for(int j =0; j<skill.length()-1; j++){
-                    String alpa1 = ""+skill.charAt(j);
-                    String alpa2 = ""+skill.charAt(j+1);
+            String alpa1 = ""+skill.charAt(0);
+            for(int j =1; j<skill.length(); j++){                   
+                    String alpa2 = ""+skill.charAt(j);
                     int cur = skill_trees[i].indexOf(alpa1);
                     int next = skill_trees[i].indexOf(alpa2);
                     if((next != -1 && cur >= next) || (cur==-1 && next != -1)) {
                         isTrue = false;
                         break;
                     }
+                alpa1 = alpa2;
             }
             if(isTrue)answer++;
         }
