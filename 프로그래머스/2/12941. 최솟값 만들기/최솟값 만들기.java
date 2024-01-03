90e0f8d560f820874e9887efe5b5f90e6366f561
JM <jaemin6552@Naver.com>
2024-01-04 00:00:40 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.71 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -6,13 +6,14 @@ class Solution
     public int solution(int []A, int []B)
     {
         int answer = 0;
-        Arrays.sort(A);
-        PriorityQueue<Integer>pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int e : B){
-            pq.add(e);
+        PriorityQueue<Integer>pqA = new PriorityQueue<>();
+        PriorityQueue<Integer>pqB = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i<A.length; i++){
+            pqA.add(A[i]);
+            pqB.add(B[i]);
         }
-        for(int e : A){
-            answer+=e*pq.poll();
+        while(!pqA.isEmpty()){
+            answer+=pqA.poll()*pqB.poll();
         }
 
         return answer;
