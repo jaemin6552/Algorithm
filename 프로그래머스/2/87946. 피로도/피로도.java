591bea89f075435d7930a2e221c78ba4c9cd77b3
JM <jaemin6552@Naver.com>
2024-01-10 01:55:06 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -4,15 +4,15 @@ class Solution {
     public int solution(int k, int[][] dungeons) {
         answer = 0;
         visited = new boolean[dungeons.length];
-        dfs(k,dungeons,0,0);
+        dfs(k,dungeons,0);
         return answer;
     }
-    public void dfs(int k,int[][] dungeons,int start,int cnt){
+    public void dfs(int k,int[][] dungeons,int cnt){
         int max = cnt;
         for(int i =0; i<dungeons.length; i++){
             if(!visited[i] && k >= dungeons[i][0]){
             visited[i] = true;
-            dfs(k-dungeons[i][1],dungeons,i+1,max+1);
+            dfs(k-dungeons[i][1],dungeons,max+1);
             visited[i] = false;
             }
         }
