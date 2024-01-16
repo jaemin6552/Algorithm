c44a59db49d7ce273b125c93ca1c42f7a408dbcd
JM <jaemin6552@Naver.com>
2024-01-17 01:08:47 +0900

    [level Lv.3] Title: 풍선 터트리기, Time: 216.44 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/Lv.3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/Lv.3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -2,20 +2,16 @@ import java.util.*;
 class Solution {
     public int solution(int[] a) {
         int answer = 0;
-        List<Integer>left = new ArrayList<>();
-        List<Integer>right = new ArrayList<>();
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MAX_VALUE;
         int a_length = a.length-1;
-        for(int i = 0; i<a.length; i++){
-            if(left.isEmpty())left.add(a[i]);
-            else left.add(left.get(i-1) > a[i] ? a[i] : left.get(i-1));
-        }
-        for(int i = 0; i<a.length; i++){
-            if(right.isEmpty())right.add(a[a_length-i]);
-            else right.add(right.get(i-1) > a[a_length-i] ? a[a_length-i] : right.get(i-1));
-        }
+        HashSet<Integer>hashset = new HashSet<>();
         for(int i =0; i<=a_length; i++){
-            if(a[i] <= left.get(i) || a[i] <= right.get(a_length-i)) answer++;
-        }      
-        return answer;
+            left = Math.min(left,a[i]);
+            right = Math.min(right,a[a_length-i]);
+            hashset.add(left);
+            hashset.add(right);
+        }
+        return hashset.size();
     }
 }
