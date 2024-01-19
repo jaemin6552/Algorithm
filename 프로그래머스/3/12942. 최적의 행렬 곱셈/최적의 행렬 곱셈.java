d9841aef33254b18dd30a7cd95f482ea8109f1ad
JM <jaemin6552@Naver.com>
2024-01-20 06:13:01 +0900

    [level 3] Title: 최적의 행렬 곱셈, Time: 27.13 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.java
+++ 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.java
@@ -1,25 +1,22 @@
 import java.util.*;
 class Solution {
-    int answer;
-    boolean []visited;
     public int solution(int[][] matrix_sizes) {
-        answer = 0;
-        int size = matrix_sizes.length;
-        int [][]dp = new int[size][size];
-        for(int []e : dp) Arrays.fill(e,Integer.MAX_VALUE);
-        for(int i =0; i<dp.length; i++)dp[i][i] = 0;
+        int answer = 0;
+        int m_size = matrix_sizes.length;
+        int [][]dp = new int[m_size][m_size];
+        for(int [] tmp : dp) Arrays.fill(tmp,Integer.MAX_VALUE);
+        for(int i =0; i<dp.length; i++) dp[i][i] = 0;
         
-        for(int i = 0; i<size; i++){
-            for(int j=0; j<size-i; j++){
+        for(int i =0; i<m_size; i++){
+            for(int j =0; j<m_size-i; j++){
                 int a = j;
-                int b = j + i;
-                for(int k = a; k<b; k++){
-                    dp[a][b] = Math.min(dp[a][b],dp[a][k]+dp[k+1][b]+
-                        matrix_sizes[a][0]*matrix_sizes[k+1][0]*matrix_sizes[b][1]);
+                int b = j+i;
+                for(int k=a;k<b;k++){
+                    dp[a][b]=Math.min(dp[a][b],dp[a][k]+dp[k+1][b]
+                                      +matrix_sizes[a][0]*matrix_sizes[k][1]*matrix_sizes[b][1]);
                 }
             }
         }
-        return dp[0][size-1];
+        return dp[0][m_size-1];
     }
-    
 }
